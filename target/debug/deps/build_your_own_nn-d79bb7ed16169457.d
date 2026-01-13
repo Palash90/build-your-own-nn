4e762cb42cd2888051f2696d080716151b5f67df
palash90 <palash.kanti.kundu.90@gmail.com>
2026-01-13 13:59:55 +0530

    Initial Repository

---
+++ target/debug/deps/build_your_own_nn-d79bb7ed16169457.d
@@ -0,0 +1,5 @@
+/home/palash/git/build-your-own-nn/target/debug/deps/build_your_own_nn-d79bb7ed16169457.d: src/main.rs
+
+/home/palash/git/build-your-own-nn/target/debug/deps/libbuild_your_own_nn-d79bb7ed16169457.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/build_your_own_nn-f726422437c624a3.d
@@ -0,0 +1,5 @@
+/home/palash/git/build-your-own-nn/target/debug/deps/build_your_own_nn-f726422437c624a3.d: src/main.rs
+
+/home/palash/git/build-your-own-nn/target/debug/deps/libbuild_your_own_nn-f726422437c624a3.rmeta: src/main.rs
+
+src/main.rs:
